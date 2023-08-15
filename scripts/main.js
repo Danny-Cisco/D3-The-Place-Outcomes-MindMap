ffb218918530ed652a8671ccdac2966bfba63612
Danny-Cisco <gumballelectronics@gmail.com>
2023-08-15 17:53:20 +1000

    listener for resize

--- scripts/main.js
+++ scripts/main.js
@@ -11,10 +11,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
             createChart(data);
         });
     }
-    
+
+    // This is your new handleResize function
+    function handleResize() {
+        // Recreate the chart using current window width
+        fetchDataForSelectedFile();
+    }
 
     // Event listener for the JSON file dropdown selection
     document.getElementById("json-selector").addEventListener("change", fetchDataForSelectedFile);
+
+    // Event listener for window resizing
+    window.addEventListener('resize', handleResize);
 
     // Initial load of default data
     fetchDataForSelectedFile();
