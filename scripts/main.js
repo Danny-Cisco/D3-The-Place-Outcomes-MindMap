bdb4e7b9d43cfba80ae8e5be4355dab3bc8037da
Danny-Cisco <gumballelectronics@gmail.com>
2023-08-15 15:22:36 +1000

    remove old svg

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     
-    // Fetch data based on selected JSON file
     function fetchDataForSelectedFile() {
         const selectedFile = document.getElementById("json-selector").value;
+    
+        // Remove the old SVG if it exists
+        d3.select("svg").remove();
+    
         fetchData(selectedFile).then(data => {
             console.log(data);
             createChart(data);
         });
     }
+    
 
     // Event listener for the JSON file dropdown selection
     document.getElementById("json-selector").addEventListener("change", fetchDataForSelectedFile);
